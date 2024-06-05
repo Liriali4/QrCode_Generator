dd71cae48bc052920a00e146cc50dcde5edf191e
Liriali4 <lirialia51@gmail.com>
2024-06-05 18:12:39 +0100

    adicionadas  as configurações do firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDreskiKo5uEC35Z20P2GDfoIRNKA5oFBU",
+  authDomain: "qrcodegenerator-9c9cd.firebaseapp.com",
+  projectId: "qrcodegenerator-9c9cd",
+  storageBucket: "qrcodegenerator-9c9cd.appspot.com",
+  messagingSenderId: "545773174157",
+  appId: "1:545773174157:web:dc9ff43709f1f9451951c4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Box, Input, Button, Flex } from "@chakra-ui/react";
 import {
     FormControl,
